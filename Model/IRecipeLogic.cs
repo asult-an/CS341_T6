09aae71562d0dc29f08489fdabf504ecc2e82485
unknown <schnei71@uwosh.edu>
2023-10-28 16:28:31 -0500

    Added RecipeLogic with some error handling

---
+++ Model/IRecipeLogic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CookNook.Model
+{
+    public interface IRecipeLogic
+    {
+        RecipeAdditionError CreateRecipe(int inId, string inName, string inDescription, int inAuthor,
+            ObservableCollection<string> inIngredients, ObservableCollection<string> inIngredientsQty,
+            int inCooktime, string inCourse, int inRating, int inServings, string inImage,
+            ObservableCollection<string> inTags, ObservableCollection<string> inFollowers);
+
+        RecipeAdditionError AddRecipe(Recipe recipe);
+
+        RecipeEditError EditRecipe(Recipe recipe);
+
+        RecipeDeletionError DeleteRecipe(Recipe recipe);
+
+        Recipe FindRecipe(int id);
+    }
+}
+
