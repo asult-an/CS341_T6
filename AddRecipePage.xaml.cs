7b0d447377999ee8ac3f143f52583a39dd65a7ff
asult-an <sultaa03@uwosh.edu>
2023-11-26 16:23:06 -0600

    Changed AddRecipePage CookTime Entry to numeric

--- AddRecipePage.xaml.cs
+++ AddRecipePage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -11,36 +12,57 @@ using Microsoft.Maui.Storage;
 
 namespace CookNook
 {
-    public partial class AddRecipePage : ContentPage
+    public partial class AddRecipePage : ContentPage, INotifyPropertyChanged
     {
         private string recipeName;
         private string recipeCooktime;
         private string recipeInstructions;
-
-        //private string Description
-        private int Servings;
+        // private string description
+        // private int servings;
         private IRecipeLogic recipeLogic;
         private IIngredientLogic ingredientLogic;
-
-        // TODO: PreviousPageData uses these three.  Are we grabbing these?
-        public string RecipeName { get { return recipeName; } set { recipeName = value; } }
-        public string RecipeCooktime { get { return recipeCooktime; } set { recipeCooktime = value; } }
-        public string RecipeInstructions { get { return recipeInstructions; } set { recipeInstructions = value; } }
         private string imagePath;
         private byte[] imageBytes;
         private User user;
 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // TODO: PreviousPageData uses these three.  Are we grabbing these?
+        public string RecipeName { get { return recipeName; } set { recipeName = value; } }
+        public string RecipeCooktime { get { return recipeCooktime; } set { recipeCooktime = value; } }
+
+        /// <summary>
+        /// The instructions for the recipe, bound to the Description inpue in the XAML
+        /// </summary>
+        public string RecipeInstructions
+        {
+            get { return recipeInstructions; }
+            set
+            {
+                // if not null, set the value
+                if (recipeInstructions != value)
+                {
+                    recipeInstructions = value;
+                    OnPropertyChanged(nameof(RecipeInstructions));
+                }
+            }
+        }
+
+
         // constructor with dependency injection
-        public AddRecipePage(IRecipeLogic recipeLogic)
+        public AddRecipePage(IRecipeLogic recipeLogic, IIngredientLogic ingredientLogic)
         {
             InitializeComponent();
             this.recipeLogic = recipeLogic;
+            this.ingredientLogic = ingredientLogic;
+            user = UserViewModel.Instance.AppUser;
         }
 
         public AddRecipePage()
         {
             InitializeComponent();
             this.recipeLogic = new RecipeLogic(new RecipeDatabase());
+            this.ingredientLogic = new IngredientLogic(new IngredientDatabase());
             user = UserViewModel.Instance.AppUser;
         }
 
@@ -48,11 +70,14 @@ namespace CookNook
         private async void NextClicked(object sender, EventArgs e)
         {
             // warn the user if they try to enter a recipe with missing datum
-            if (Name.Text == null || TimeToMake.Text == null || recipeInstructions == null)
+            // TODO: write better sanity checks
+            //if (Name.Text == null || TimeToMake.Text == null || recipeInstructions == null)
+            if (Name.Text == null || TimeToMake.Text == null || Description.Text == null)
             {
                 await DisplayAlert("Error", "Please fill out all fields", "Okay");
                 return;
             }
+            recipeInstructions = Description.Text;
 
             try
             {
@@ -73,7 +98,7 @@ namespace CookNook
                 Debug.Write("AddRecipePage: " + ex.Message);
                 DisplayAlert("Error", "Recipe Add Failed", "Okay");
             }
-            
+
         }
 
         private async void PickImageClicked(object sender, EventArgs e)
@@ -112,7 +137,9 @@ namespace CookNook
             }
         }
 
-
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
-
 }
