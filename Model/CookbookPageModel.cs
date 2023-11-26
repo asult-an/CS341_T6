f516cac66bc42ef78917d124da1a80660939e615
Vincent <morriv92@uwosh.edu>
2023-11-26 15:42:24 -0600

    feature: added optional List<Recipe> property on CookbookPageModel

--- Model/CookbookPageModel.cs
+++ Model/CookbookPageModel.cs
@@ -15,23 +15,27 @@ namespace CookNook.Model
         private string pageName;
         private long userId;
         private long listId;
-
+        private List<Recipe> recipes;
 
         public string PageName { get { return pageName; } set { pageName = value; } }
         public long UserId { get { return userId; } set { userId = value; } }
         public long ListId { get { return listId; } set { listId = value; } }   
         
+        public List<Recipe> Recipes { get { return recipes; } set { recipes = value; } }
+
         /// <summary>
         /// Constructor for CookbookPageModel
         /// </summary>
         /// <param name="pageName"></param>
         /// <param name="userId"></param>
         /// <param name="listId"></param>
-        public CookbookPageModel(string pageName, long userId, long listId)
+        public CookbookPageModel(string pageName, long userId, long listId, List<Recipe> recipes = null)
         {
             this.pageName = pageName;
             this.userId = userId;
             this.listId = listId;
+            // set recipes to an empty list if it wasn't provided
+            this.recipes = (recipes != null) ? recipes: new List<Recipe>();
         }
 
     }
