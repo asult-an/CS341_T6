c3e17e59b1760b8bb268c6f5c4da850b16f05860
Vincent <morriv92@uwosh.edu>
2023-11-27 20:36:48 -0600

    add: added missing IUserLogic interface fix: implemented unimplemented GetUserByEmail

--- Model/RecipeLogic.cs
+++ Model/RecipeLogic.cs
@@ -1,71 +1,133 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CookNook.Model.Interfaces;
+using CookNook.Services;
 
 namespace CookNook.Model
 {
     internal class RecipeLogic : IRecipeLogic
     {
-        private RecipeDatabase recipeDatabase;
+        private List<Recipe> recipes;
 
-        public RecipeLogic()
+        const int MAX_RECIPE_NAME_LENGTH = 50;
+        const int MAX_RECIPE_DESCRIPTION_LENGTH = 150;
+        private IIngredientLogic ingredientLogic;
+        private IRecipeDatabase recipeDatabase;
+        public List<Recipe> Recipes
         {
-            recipeDatabase = new RecipeDatabase();
+            get { return recipes; }
+            set { recipes = value; }
         }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RecipeLogic"/> class, with the database service injected
+        /// </summary>
+        /// <param name="recipeDatabase">The service for recipe-based database interactions</param>
+        public RecipeLogic(IRecipeDatabase recipeDatabase, IIngredientLogic ingredientLogic)
+        {
+            this.recipeDatabase = recipeDatabase;
+            this.ingredientLogic = ingredientLogic;
+        }
+
+     
 
        // this method may be redundant
-       public RecipeAdditionError CreateRecipe(int inId, string inName, string inDescription, int inAuthor,
-            ObservableCollection<string> inIngredients, ObservableCollection<string> inIngredientsQty,
-            int inCooktime, string inCourse, int inRating, int inServings, string inImage,
-            ObservableCollection<string> inTags, ObservableCollection<string> inFollowers)
-        {
-            if (string.IsNullOrEmpty(inName))
+       public RecipeAdditionError CreateRecipe(string inName, string inDescription, Int64 inAuthorId,
+           string inIngredients, string inIngredientsQty,
+           int inCooktime, string inCourse, int inRating, int inServings, string inImage,
+           string inTags, string inFollowers)
+
+       {
+
+            //TODO: ingredients will become Ingredient[] instead of a spring 
+
+
+            // validate name
+            if (string.IsNullOrEmpty(inName) || inName.Length > MAX_RECIPE_NAME_LENGTH)
                 return RecipeAdditionError.InvalidName;
 
-            // Validate description is not too long
-            if (inDescription.Split(' ').Length > 150)
+            // Validate description
+            if (inDescription.Split(' ').Length > MAX_RECIPE_DESCRIPTION_LENGTH)
                 return RecipeAdditionError.InvalidDescription;
 
+            // Q: how do we resolve Id back from the Database (since it's returning a RecipeError) if it's automatically generated
+            // A: The returned object from the query may have something to answer this, otherwise we could do a SELECT FROM RECIPES WHERE...
             
-            
-
             // If all validations pass, construct the Recipe object
-            Recipe newRecipe = new Recipe(inId, inName, inDescription, inAuthor, inIngredients,
-                inIngredientsQty, inCooktime, inCourse, inRating, inServings, inImage, inTags, inFollowers);
+            Recipe newRecipe = new Recipe
+            {
+                AuthorID = inAuthorId,
+                CookTime = inCooktime,
+                Course = CourseType.Parse(inCourse),
+                Description = inDescription,
+                // new recipe will have no followers
+                //FollowerIds = recipeDatabase.GetIngredientsByRecipe(),
+                
+                Image = Encoding.ASCII.GetBytes(inImage),
+            }; 
 
-           
-            AddRecipe(newRecipe);
-            return RecipeAdditionError.NoError;
+            return AddRecipe(newRecipe);
         }
 
 
-        public RecipeAdditionError AddRecipe(Recipe recipe)
+       public RecipeAdditionError CreateRecipe(Int64 inId, string inName, string inDescription, Int64 inAuthorID, string inIngredients,
+           string inIngredientsQty, int inCooktime, string inCourse, int inRating, int inServings, string inImage,
+           string inTags, string inFollowers)
+       {
+           return RecipeAdditionError.NoError;
+       }
+
+       /// <summary>
+        /// Adds a new recipe to the database, with no followers.
+        /// </summary>
+        /// <param name="recipe"></param>
+        /// <returns>Appropriated error if the add failed</returns>
+       public RecipeAdditionError AddRecipe(Recipe recipe)
         {
+            // check for duplicate Id
             if (FindRecipe(recipe.ID) != null)
                 return RecipeAdditionError.DuplicateId;
 
+            if (recipe.Description == null)
+                recipe.Description = "";
+
+            if (recipe.Image == null)
+                recipe.Image = Encoding.ASCII.GetBytes("NO_IMAGE");
+
             try
             {
-                return recipeDatabase.InsertRecipe(recipe);
+                recipeDatabase.InsertRecipe(recipe);
+                return RecipeAdditionError.NoError;
             }
             catch (Exception ex)
             {
-                // returning a generic error.
+
+                Debug.WriteLine(ex.ToString());
+                // This is where adding a recipe is failing, its catching some exception 
+                // in the database, and I'm not sure what the issue is.
+
                 return RecipeAdditionError.DBAdditionError;
             }
         }
 
         public RecipeEditError EditRecipe(Recipe recipe)
         {
-            if (FindRecipe(recipe.ID) == null)
-                return RecipeEditError.RecipeNotFound;
+            //if (FindRecipe(recipe.ID) == null)
+            //    return RecipeEditError.RecipeNotFound;
 
             try
             {
-                return recipeDatabase.EditRecipe(recipe);
+                var target = recipes.Find(r => r.ID == recipe.ID);
+                if (target == null)
+                    return RecipeEditError.RecipeNotFound;
+
+                recipeDatabase.EditRecipe(recipe);
+                return RecipeEditError.NoError;
             }
             catch (Exception ex)
             { 
@@ -77,7 +139,10 @@ namespace CookNook.Model
         {
             try
             {
-                return recipeDatabase.DeleteRecipe(recipe);
+
+                recipeDatabase.DeleteRecipe(recipe.ID);
+                return RecipeDeletionError.NoError;
+
             }
             catch (Exception ex)
             {
@@ -85,18 +150,102 @@ namespace CookNook.Model
             }
         }
 
-        public Recipe FindRecipe(int id)
+        public List<Recipe> SelectRecipeByCooktime(int cooktime)
+        {
+            // if cooktime is invalid...
+
+            return recipeDatabase.SelectRecipeByCooktime(cooktime);
+        }
+
+        /// <summary>
+        /// Grabs a list of recipes by their ids
+        /// </summary>
+        /// <param name="recipeList"></param>
+        /// <returns>List of recipes</returns>
+        public List<Recipe> SelectRecipes(List<Int64> recipeList)
+        {
+            if (recipeList == null || recipeList.Count == 0)
+                throw new ArgumentException("recipeList cannot be null or empty");
+
+            if (recipeList.Any(id => FindRecipe(id) == null))
+                throw new ArgumentException("recipeList contains invalid recipe Ids");
+            
+            return recipeDatabase.SelectRecipes(recipeList);
+        }
+
+        /// <summary>
+        /// Selects a recipe by its Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Recipe object, else null</returns>
+        public Recipe FindRecipe(Int64 id)
         {
             try
             {
-                // will have to pass id once database method is implemented
-                return recipeDatabase.SelectRecipe();
+                return recipeDatabase.SelectRecipe(id);
             }
-            catch
+            catch (Exception ex)
             {
+                Debug.WriteLine("Error finding recipe: " + ex);
                 return null;
             }
         }
+
+        public List<Int64> GetFollowerIds(Int64 recipeID)
+        {
+            return recipeDatabase.GetRecipeFollowerIds(recipeID);
+        }
+
+
+        public List<Tag> GetTagsForRecipe(Int64 recipeID)
+        {
+            // TODO: might need to return error rather than propogate the Tag up
+            // if recipeID is not valid:
+            return recipeDatabase.GetTagsForRecipe(recipeID);
+            // throw new NotImplementedException();
+        }
+
+        public List<Recipe>? GetRecipesByUserId(Int64 userID)
+        {
+            try
+            {
+                return recipeDatabase.GetRecipesByUserId(userID);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public ObservableCollection<Recipe> CookBookRecipes(long userID)
+        {
+            try
+            {
+                return recipeDatabase.CookbookRecipes(userID);
+            } catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+
+
+        public List<Recipe>? SelectAllRecipes() 
+        {
+            try
+            {
+                // get by user...?
+
+                return recipeDatabase.SelectAllRecipes();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error selecting all recipes", ex);
+                return null;
+            }
+        }
+
     }
 }
 
--- Model/UserLogic.cs
+++ Model/UserLogic.cs
@@ -1,6 +1,8 @@
-﻿using Npgsql;
+﻿using CookNook.Services;
+using Npgsql;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +10,16 @@ using System.Threading.Tasks;
 //namespace CookNook.Services
 namespace CookNook.Model
 {
-    internal class UserLogic : IUserLogic
+
+    public class UserLogic
     {
-        // place for the injected database instance to load into
+        Random random = new Random();
+
+        // place for the injected datbase instance to load into
         private readonly IUserDatabase userDatabase;
+
+        // TODO: check to see if we can justify keeping this, or if userDatabase can be utilized
+        //private UserDatabase userDatabase = new UserDatabase();
 
         // since users can interact with recipies, inject RecipeLogic
         // may not use it now, but by doing this we can send recipe data to users
@@ -32,6 +40,7 @@ namespace CookNook.Model
         {
             this.userDatabase = userDatabase;
         }
+        public UserLogic() { }
 
         /// <summary>
         /// 
@@ -40,15 +49,51 @@ namespace CookNook.Model
         /// <param name="recipeLogic"></param>
         public UserLogic(IUserDatabase userDatabase, IRecipeLogic recipeLogic)
         {
-
+            this.userDatabase = userDatabase;
+            this.recipeLogic = recipeLogic;
         }
-
-
-        public bool RegisterNewUser(string username, string email)
+        public UserAuthenticationError AuthenticateUser(string username, string password)
         {
-            return false;
+           return userDatabase.AuthenticateUser(username, password);
         }
-        
+
+        public UserAdditionError RegisterNewUser(string username, string email, string password, string confirmPassword)
+        {
+            //TODO: add email confirmation
+            if(!ValidateSignupInput(username, email, password, confirmPassword))
+            {
+                return UserAdditionError.InvalidPassword;
+            }
+            // Note:  we want the database to come up with Id, since we're wasting computation if the 
+            // insert is going to fail
+            User newUser = new User((Int64)random.NextInt64(5000), username, email, password);
+            try
+            {
+                UserAdditionError result = userDatabase.InsertUser(newUser);
+                if (result != UserAdditionError.NoError) 
+                {
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+                return UserAdditionError.DBAdditionError;
+            }
+            return UserAdditionError.NoError;
+        }
+        private bool ValidateSignupInput(string username, string email, string password, string confirmPassword)
+        {
+
+            if(password != confirmPassword)
+            {
+                return false;
+            }
+            //TODO: Add profanity filter
+            //TODO: add special character invalidator
+            return true;
+        }
+
         /// <summary>
         /// Attempt to follow a recipe if not already following.  If
         /// 
@@ -56,7 +101,7 @@ namespace CookNook.Model
         /// <param name="userId"></param>
         /// <param name="recipeId"></param>
         /// <returns></returns>
-        public UserSelectionError FollowRecipe(int userId, int recipeId)
+        public UserSelectionError FollowRecipe(Int64 userId, Int64 recipeId)
         {
             /* if recipeLogic injected:
              * check if recipeID is valid
@@ -67,32 +112,51 @@ namespace CookNook.Model
 
         }
 
-        public List<User> GetAllUsers()
+        public List<User> GetUsersByID(List<Int64> userIDs)
         {
-            return userDatabase.GetAllUsers();
+            return userDatabase.GetUsersById(userIDs);
         }
 
         public User GetUserByEmail(string email)
         {
+            // verify that the email matches the format of an email address
+
+
             return userDatabase.GetUserByEmail(email);
         }
 
-        public User GetUserById(int id)
+        public User GetUserById(Int64 id)
         {
+
             return userDatabase.GetUserById(id);
         }
 
+        public User GetUserByUsername(string username)
+        {
+            
+            //return userDatabase.GetUserByUsername(username);
+        }
 
         public UserAdditionError InsertUser(User inUser)
         {
             return userDatabase.InsertUser(inUser);
         }
 
-        public UserEditError EditUser(User inUser)
-        {
-            return userDatabase.EditUser(inUser);
+        //public UserEditError UpdateUserInfo(Int64 id, string username, string password, string imgPath)
+        //{
+        //    return userDatabase.UpdateUserInfo(id, username, password, imgPath);
 
-        }
+        //}
+
+        //public UserEditError UpdateFollowedUser(string followedId, string followerId)
+        //{
+
+        //}
+
+        //public UserSelectionError GetFollowedUsers(int userId)
+        //{
+
+        //}
 
         public UserDeletionError DeleteUser(User inUser)
         {
