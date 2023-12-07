7fe1e3de880eb88ff0ee11aa39c28f3afdf0b492
Vincent <morriv92@uwosh.edu>
2023-12-07 14:59:24 -0600

    edit: tweaked role of IPreferenceDatabase add: added DietAffectedIngredient

---
+++ Model/DietAffectedIngredient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookNook.Model
+{
+    /// <summary>
+    /// Represents an ingredient subjected to the constraints of a user's preference.
+    /// To avoid decoupling or data de-synchronization, we only store the referenced ingredient's ID
+    /// </summary>
+    public class DietAffectedIngredient
+    {
+
+        /// <summary>
+        /// The Id of the recipe that the affected ingredient is storing
+        /// </summary>
+        public long IngredientId { get; private set; }
+
+        public bool IsPreferred { get; private set; }
+
+
+        /// <summary>
+        /// Constructs a new DietAffectedRecipe to represent a row from 'preferences_recipes'
+        /// </summary>
+        /// <param name="recipeId"></param>
+        /// <param name="isPreferred"></param>
+        public DietAffectedIngredient(long ingredientId, bool isPreferred)
+        {
+            IngredientId = ingredientId;
+            IsPreferred = isPreferred;
+        }
+    }
+}
--- Model/Interfaces/IPreferenceDatabase.cs
+++ Model/Interfaces/IPreferenceDatabase.cs
@@ -14,24 +14,61 @@ namespace CookNook.Model.Interfaces
     /// </summary>
     public interface IPreferenceDatabase
     {
-        public List<DietPreference> GetPreferencesFor(long userId);
-
-
+        /// <summary>
+        /// Used to grab all of the latest settings from both ingredients and recipes
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public Task<List<DietPreference>> GetPreferencesAsync(long userId);
+        
         /// <summary>
         /// Overwrites all preferences for a user in bulk: this way PreferenceProvider can 
         /// check against the locally stored settings to trim out data from the proposed 
         /// query that would otherwise be redundant.
         /// </summary>
-        /// <param name="userId"></param>
+        /// <param name="userId">the user who's updating their preferences</param>
         /// <param name="preferences"></param>
         /// <returns></returns>
         public bool UpdatePreferencesFor(long userId, List<DietPreference> preferences);
+
+
+        /// <summary>
+        /// Retrieves the ingredients affected by a particular user's preference
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="prefId"></param>
+        /// <returns></returns>
+        // public List<DietAffectedIngredient> GetPrefAffectedIngredients(long userId, long prefId);
+
+
+        /// <summary>
+        /// Retrieves the recipes affected by a particular user's preference
+        /// </summary>
+        /// <param name="userId">id of which user's preferences we want</param>
+        /// <returns></returns>
+        // public List<DietAffectedRecipe> GetGetPrefAffectedRecipes(long userId);
+
+        /// <summary>
+        /// Retrieves all ingredients that are affected by the user's preferences
+        /// </summary>
+        /// <param name="userId">id of which user's preferences we want</param>
+        /// <returns></returns>
+        public List<DietAffectedIngredient> GetAffectedIngredients(long userId);
+
+
+        /// <summary>
+        /// Retrieves all recipes that are affected by the user's preferences
+        /// </summary>
+        /// <param name="userId">id of which user's preferences we want</param>
+        /// <returns></returns>
+        public List<DietAffectedRecipe> GetAffectedRecipes(long userId, long prefId);
+
 
         /// <summary>
         /// Wipes all of the stored preferences for a user in the user_settings table
         /// </summary>
         /// <param name="userId"></param>
         /// <returns></returns>
-        public bool DeletePreferencesFor(long userId);
+        public bool DeleteAllPreferencesFor(long userId);
     }
 }
